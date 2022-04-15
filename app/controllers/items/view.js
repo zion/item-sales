68b9a0f5e06e2f6b546d1a641d64fbee169df95a
Simeon Berns <simeonberns@littledesk.local>
2022-04-15 11:42:12 -0500

    Added workaround for strange routing issuae

--- app/controllers/items/view.js
+++ app/controllers/items/view.js
@@ -9,32 +9,34 @@ export default class ItemsViewController extends Controller {
   @action
   deleteItem(item) {
     item.destroyRecord().then(() => {
-      this.router.transitionTo('items.index');
+      this.transitionToRoute('/');
     });
   }
 
   get weekly_sales() {
     let sales_by_week = {};
-    this.model.daily_item_sale.map((sale) => {
-      dayjs.extend(WeekOfYear);
-      const dow = dayjs(sale.business_day).startOf('w').$d;
-      const date = dayjs(dow).format('MM/DD/YYYY');
-      // if first day of week is not in object, create with first sale
-      // else add values
-      if (sales_by_week[date]) {
-        sales_by_week[date]['total_qty'] += sale.total_qty;
-        sales_by_week[date]['total_sales'] += sale.total_sales;
-        sales_by_week[date]['total_cost'] += sale.total_cost;
-      } else {
-        sales_by_week[date] = {
-          business_date: date,
-          total_sales: sale.total_sales,
-          total_cost: sale.total_cost,
-          total_qty: sale.total_qty,
-        };
-      }
-    });
-    let arr = Object.keys(sales_by_week).map((k) => sales_by_week[k]);
-    return arr;
+    if (!this.model.isDeleted) {
+      this.model.daily_item_sale.map((sale) => {
+        dayjs.extend(WeekOfYear);
+        const dow = dayjs(sale.business_day).startOf('w').$d;
+        const date = dayjs(dow).format('MM/DD/YYYY');
+        // if first day of week is not in object, create with first sale
+        // else add values
+        if (sales_by_week[date]) {
+          sales_by_week[date]['total_qty'] += sale.total_qty;
+          sales_by_week[date]['total_sales'] += sale.total_sales;
+          sales_by_week[date]['total_cost'] += sale.total_cost;
+        } else {
+          sales_by_week[date] = {
+            business_date: date,
+            total_sales: sale.total_sales,
+            total_cost: sale.total_cost,
+            total_qty: sale.total_qty,
+          };
+        }
+      });
+      let arr = Object.keys(sales_by_week).map((k) => sales_by_week[k]);
+      return arr;
+    }
   }
 }
