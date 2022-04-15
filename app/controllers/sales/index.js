f5fb88c32be7e4e63b230b8ec0674149d9797e26
Simeon <simeonberns@gmail.com>
2022-04-15 08:40:42 -0500

    Refactoring to object

--- app/controllers/sales/index.js
+++ app/controllers/sales/index.js
@@ -6,22 +6,28 @@ import { inject as service } from '@ember/service';
 export default class SalesIndexController extends Controller {
   @service intl;
   get sales() {
-    let sales_by_week = [];
+    let sales_by_week = {};
     this.model.map((sale) => {
-      let week = this.intl.formatDate(sale.business_day);
       dayjs.extend(WeekOfYear);
-      const woy = dayjs(sale.business_day).week();
-      if (sales_by_week[woy]) {
-        // increase sales
-      } else {
-        // add week to array
-        sales_by_week[woy] = {
-          business_week: dayjs(sale.business_day).startOf('w').$d,
-          total_sales: sale.total_sales,
-          total_cost: sale.total_cost,
-        };
+      const woy = dayjs(sale.business_day).week().toString();
+      const y = dayjs(sale.business_day).year();
+      // debugger
+      if (sales_by_week[y] === undefined) {
+        // Add year to array
+        sales_by_week[y] = {};
       }
-      // const woy = dayjs(sale.business_day).week();
+
+      // if (sales_by_week[y][woy] === undefined) {
+
+      // }
+      // add week to array
+
+      // debugger
+      // sales_by_week[y][woy] = {
+      //   business_week: dayjs(sale.business_day).startOf('w').$d,
+      //   total_sales: sale.total_sales,
+      //   total_cost: sale.total_cost,
+      // };
     });
     debugger
     return sales_by_week;
