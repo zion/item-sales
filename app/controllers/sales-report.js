4f37ea334d5117302d07b923e0186416c89165ec
Simeon <simeonberns@gmail.com>
2022-05-25 14:44:23 -0500

    Refactor sales report route to use new api endpoint

--- app/controllers/sales-report.js
+++ app/controllers/sales-report.js
@@ -1,33 +1,19 @@
 import Controller from '@ember/controller';
-import dayjs from 'dayjs';
-import WeekOfYear from 'dayjs/plugin/weekOfYear';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class SalesReportController extends Controller {
-  get sales() {
-    let sales_by_week = {};
-    this.model.map((sale) => {
-      dayjs.extend(WeekOfYear);
-      const dow = dayjs(sale.businessDay).startOf('w').$d;
-      const date = dayjs(dow).format('MM/DD/YYYY');
-      // if first day of week is not in object, create with first sale
-      // else add values
-      if (sales_by_week[date]) {
-        sales_by_week[date]['total_qty'] += sale.totalQty;
-        sales_by_week[date]['total_sales'] += sale.totalSales;
-        sales_by_week[date]['total_cost'] += sale.totalCost;
-      } else {
-        sales_by_week[date] = {
-          business_date: date,
-          total_sales: sale.totalSales,
-          total_cost: sale.totalCost,
-          total_qty: sale.totalQty,
-        };
-      }
-    });
-    let arr = Object.keys(sales_by_week).map((k) => sales_by_week[k]);
-    let sorted_arr = arr.sort((a, b) => {
-      return dayjs(b.business_date).$d - dayjs(a.business_date).$d;
-    });
-    return sorted_arr;
+  queryParams = ['sort', 'sort_direction'];
+  @tracked sort = null;
+  @tracked sort_direction = null;
+
+  @action
+  changeSortColumn(column) {
+    this.sort = column;
+    if (this.sort_direction == 'ASC') {
+      this.sort_direction = 'DESC';
+    } else {
+      this.sort_direction = 'ASC';
+    }
   }
 }
--- app/routes/sales-report.js
+++ app/routes/sales-report.js
@@ -1,9 +1,26 @@
 import Route from '@ember/routing/route';
+import ENV from 'item-sales/config/environment';
 
 export default class SalesReportRoute extends Route {
-  model() {
-    return fetch('/aggregate-sales').then((response) => {
+  queryParams = {
+    sort: {
+      refreshModel: true,
+    },
+    sort_direction: {
+      refreshModel: true,
+    },
+  };
+
+  model(params) {
+    let url = `${ENV.APP.HOST_URL}/aggregate-sales`;
+    if (params.sort) {
+      url += `?sort=${params.sort}&sort_direction=${params.sort_direction}`;
+    }
+    return fetch(url).then((response) => {
       return response.json();
     });
   }
+  // model(params) {
+  //   return this.store.query('aggregate-sale', params);
+  // }
 }
